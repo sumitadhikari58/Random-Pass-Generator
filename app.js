30c2c3adcacbbe6e11b07b92efd19b9c2db135d6
justsumit58 <158418036+justsumit58@users.noreply.github.com>
2025-12-20 09:30:39 +0530

    JS-I commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const btnE1 =  document.querySelector(".btn")
+const inputE1 = document.getElementById("input")
+const copyE1 = document.querySelector(".fa-copy")
+copyE1.addEventListener("click",()=>{
+    copyPassword()
+})
+const alertContainerE1 = document.querySelector(".alert-container")
+btnE1.addEventListener("click",()=>{
+    createPassword()
+})
+function createPassword(){
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const passwordLength = 12
+    let password = ""
+    for(let i=0;i<passwordLength;i++){
+        const randomNumber = Math.floor(Math.random()*chars.length)
+        password += chars.substring(randomNumber,randomNumber+1)
+    }
+inputE1.value = password
+}
+function copyPassword(){
+    inputE1.select()
+    inputE1.setSelectionRange(0,9999)
+    navigator.clipboard.writeText(inputE1.value)
+   alertContainerE1.innertet = password + "copied"
+}
